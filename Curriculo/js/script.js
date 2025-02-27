4676a132c3285e97ecca5a6bf1c986acd2cae55b
luc4as-calixto <lucas_calixto-souza@estudante.sesisenai.org.br>
2025-02-27 17:24:14 -0300

    ajustes no nav e header, começo da seção sobre mim, footer ajeitado, arquivo mudados de lugar para melhor ornaginazação

--- Curriculo/js/script.js
+++ Curriculo/js/script.js
@@ -1,10 +1,17 @@
-// Detecta o rolar da página e aplica ou remove a classe 'scrolled' na navbar com id 'minha-navbar'
-window.addEventListener('scroll', function() {
+document.addEventListener('DOMContentLoaded', function () {
+
+  // Remove a classe 'animate-text' ao elemento com a classe 'animate-text' após 5 segundos
+  setTimeout(() => {
+    document.querySelector('#nome').classList.remove('animate-text');
+  }, 3900);
+
+  // Detecta o rolar da página e aplica ou remove a classe 'scrolled' na navbar com id 'minha-navbar'
+  window.addEventListener('scroll', function () {
     const navbar = document.querySelector('#minha-navbar'); // Seleciona a navbar com id 'minha-navbar'
-    if (window.scrollY > 50) { // Se o usuário rolou mais de 50px
+    if (window.scrollY > 0) { // Se o usuário rolou mais de 0px
       navbar.classList.add('scrolled'); // Adiciona a classe scrolled
     } else {
       navbar.classList.remove('scrolled'); // Remove a classe scrolled
     }
   });
-  
+});
