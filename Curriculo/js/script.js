064ba0d0eb12daf2466614175f5f0bd56c50c860
luc4as-calixto <lucas_calixto-souza@estudante.sesisenai.org.br>
2025-03-05 17:20:31 -0300

    adiciona validação de formulário de contato e melhorias no estilo CSS

--- Curriculo/js/script.js
+++ Curriculo/js/script.js
@@ -15,3 +15,33 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   });
 });
+
+document.getElementById('contato-form').addEventListener('submit', function (event) {
+  event.preventDefault(); // Evita o comportamento padrão de recarregar a página
+
+  document.getElementById('success-message').classList.add('d-none');
+
+  var whatsapp = document.getElementById('numero_contato').value;
+
+  // Remove caracteres não numéricos
+  whatsapp = whatsapp.replace(/\D/g, '');
+
+  // Verifica se o número tem exatamente 11 dígitos
+  var regex = /^[0-9]{11}$/;
+
+  if (whatsapp === '' || !regex.test(whatsapp)) {
+    document.getElementById('alert-message-erronumber').classList.remove('d-none');
+  } else {
+
+    // Oculta as mensagens de erro
+    document.getElementById('alert-message-erronumber').classList.add('d-none');
+
+    // Exibe a mensagem de sucesso
+    document.getElementById('success-message').classList.remove('d-none');
+
+    // Limpa o formulário após 2 segundos
+    setTimeout(() => {
+      document.getElementById('contato-form').reset();
+    }, 2000);
+  }
+});
